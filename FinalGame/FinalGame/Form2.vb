7574c8167ad226a57bb893f9f6db7865fd2fc6a1
kendreaditya <kendreadiya@gmail.com>
2018-04-25 23:35:00 -0700

    first commit

---
+++ FinalGame/FinalGame/Form2.vb
@@ -0,0 +1,72 @@
+﻿Public Class Form2
+    Dim front, up, down As Boolean
+    Private WithEvents Tmr As New Timer With {.Interval = 40}
+    'Private water As New Bitmap("C:\Users\Aditya Kendre\Downloads\Space_Backgrounds\meyespace_v1_01.png")
+    Private water As New Bitmap("C:\Users\Aditya Kendre\Documents\space.png")
+
+    Private waterposition As Integer = 0
+
+    Private Sub Form2_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        PictureBox1.Size = New Size(510, 510)
+        Tmr.Start()
+    End Sub
+
+    Private Sub Tmr_Tick(ByVal sender As Object, ByVal e As System.EventArgs) Handles Tmr.Tick
+        If front = True Then
+            waterposition += 100
+            If waterposition = water.Width Then waterposition = 0
+            PictureBox1.Invalidate()
+        End If
+
+        If up = True Then
+            PictureBox2.Top -= 5
+        End If
+
+        If down = True Then
+            PictureBox2.Top += 5
+        End If
+    End Sub
+
+    Private Sub PictureBox1_Paint(ByVal sender As Object, ByVal e As System.Windows.Forms.PaintEventArgs) Handles PictureBox1.Paint
+        e.Graphics.DrawImage(water, waterposition, 0)
+        If waterposition > 0 Then
+            e.Graphics.DrawImage(water, 0 - (water.Width - waterposition), 0)
+        End If
+    End Sub
+
+    Private Sub Form2_KeyDown(sender As Object, e As KeyEventArgs) Handles Me.KeyDown
+        If e.KeyCode = Keys.D Then
+            front = True
+        End If
+
+        If e.KeyCode = Keys.W Then
+            up = True
+        End If
+        If e.KeyCode = Keys.S Then
+            down = True
+        End If
+
+        If e.KeyCode = Keys.D And e.KeyCode = Keys.W Then
+            front = True
+            up = True
+        End If
+
+        If e.KeyCode = Keys.D And e.KeyCode = Keys.S Then
+            front = True
+            down = True
+        End If
+    End Sub
+
+    Private Sub Form2_KeyUp(sender As Object, e As KeyEventArgs) Handles Me.KeyUp
+        If e.KeyCode = Keys.D Then
+            front = False
+        End If
+
+        If e.KeyCode = Keys.W Then
+            up = False
+        End If
+        If e.KeyCode = Keys.S Then
+            down = False
+        End If
+    End Sub
+End Class
---
+++ FinalGame/FinalGame/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("FinalGame")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("FinalGame")>
+<Assembly: AssemblyCopyright("Copyright ©  2018")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("bbe1ce5a-3a12-44f1-ae26-460071647ff7")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
